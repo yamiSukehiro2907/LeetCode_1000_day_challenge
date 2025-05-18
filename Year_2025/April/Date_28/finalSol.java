1777bb7cc7b94f297e7d8a47d166fe0c3bb733ab
Vimal290704 <vimalyadavkr001@gmail.com>
2025-05-19 03:00:49 +0530

    LeetCode daily question may 18

--- Year_2025/April/Date_28/finalSol.java
+++ Year_2025/April/Date_28/finalSol.java
@@ -6,26 +6,26 @@ public class finalSol {
         int[] arr = { 1, 1, 1 };
         System.out.println(solution.countSubarrays(arr, (long) (5)));
     }
-}
 
-class Solution {
-    public long countSubarrays(int[] nums, long k) {
-        long total = 0, currsum = 0;
-        int left = 0;
-        int n = nums.length;
-        int right = 0;
-        for (right = 0; right < n; right++) {
-            currsum += nums[right];
-            while (getScore(left, right, currsum) >= k) {
-                currsum -= nums[left];
-                left++;
+    static class Solution {
+        public long countSubarrays(int[] nums, long k) {
+            long total = 0, currsum = 0;
+            int left = 0;
+            int n = nums.length;
+            int right = 0;
+            for (right = 0; right < n; right++) {
+                currsum += nums[right];
+                while (getScore(left, right, currsum) >= k) {
+                    currsum -= nums[left];
+                    left++;
+                }
+                total += (right - left + 1);
             }
-            total += (right - left + 1);
+            return total;
         }
-        return total;
-    }
 
-    private long getScore(int left, int right, long currsum) {
-        return ((long) (right + 1 - left) * (currsum));
+        private long getScore(int left, int right, long currsum) {
+            return ((long) (right + 1 - left) * (currsum));
+        }
     }
 }
---
+++ Year_2025/May/May_18/Test.java
@@ -0,0 +1,71 @@
+package Year_2025.May.May_18;
+
+import java.util.*;
+
+public class Test {
+    static class Solution {
+        private static int MOD = (int) 1e9 + 7;
+
+        public int colorTheGrid(int row, int column) {
+            Map<Integer, List<Integer>> validRow = new HashMap<>();
+            int totalPossiblepairKey = (int) Math.pow(3, row); // total possible pairKey
+            for (int pairKey = 0; pairKey < totalPossiblepairKey; pairKey++) {
+                int copy = pairKey; // create a copy so that is does not affect main pairKey value
+                List<Integer> temp = new ArrayList<>(); // a temp list containing possible combination
+                for (int r = 0; r < row; r++) {
+                    temp.add(copy % 3); // make sure to fill with only 0 , 1 , 2 (as there are only 3 colors)
+                    copy /= 3;
+                }
+                boolean isValidpairKey = true;
+                for (int i = 0; i < temp.size() - 1; i++) {
+                    if (temp.get(i).equals(temp.get(i + 1))) {
+                        isValidpairKey = false; // we got same color in a row
+                        break;
+                    }
+                }
+                if (isValidpairKey) {
+                    validRow.put(pairKey, temp);
+                }
+            }
+            // collecting possible pairs of rows that are possible
+            Map<Integer, List<Integer>> adjacentValidRow = new HashMap<>();
+            for (int pairKey1 : validRow.keySet()) {
+                for (int pairKey2 : validRow.keySet()) {
+                    boolean valid = true;
+                    for (int index = 0; index < row; index++) {
+                        if (validRow.get(pairKey1).get(index).equals(validRow.get(pairKey2).get(index))) {
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if (valid) {
+                        if (adjacentValidRow.containsKey(pairKey1)) {
+                            adjacentValidRow.put(pairKey1, new ArrayList<>());
+                        }
+                        // we put the validRow as key and all possible validRowsKey in list
+                        adjacentValidRow.get(pairKey1).add(pairKey2);
+                    }
+                }
+            }
+            Map<Integer, Integer> freq = new HashMap<>();
+            for (int validRowKey : validRow.keySet()) {
+                freq.put(validRowKey, 1);
+            }
+            for (int index = 1; index < column; index++) {
+                Map<Integer, Integer> temp = new HashMap<>();
+                for (int validRowKey2 : validRow.keySet()) {
+                    for (int validRowKey1 : adjacentValidRow.getOrDefault(validRowKey2, new ArrayList<>())) {
+                        temp.put(validRowKey2,
+                                (temp.getOrDefault(validRowKey2, 0) + freq.getOrDefault(validRowKey1, 0) % MOD));
+                    }
+                }
+                freq = temp;
+            }
+            int totalPair = 0;
+            for (int num : freq.values()) {
+                totalPair = (totalPair + num) % MOD;
+            }
+            return totalPair;
+        }
+    }
+}
