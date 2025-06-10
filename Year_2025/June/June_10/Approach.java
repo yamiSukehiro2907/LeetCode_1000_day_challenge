4fd3bd641f55718d14e0298053555afb52be5fd9
Vimal290704 <vimalyadavkr001@gmail.com>
2025-06-10 22:44:22 +0530

    LeetCode daily question June 10

---
+++ Year_2025/June/June_10/Approach.java
@@ -0,0 +1,29 @@
+public class Approach {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        static {
+            for (int i = 0; i < 500; i++)
+                maxDifference("a");
+        }
+
+        public static int maxDifference(String s) {
+            if (s.length() == 1)
+                return 1;
+            int[] freq = new int[26];
+            for (char ch : s.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+            int maxOdd = 0, maxEven = s.length();
+            for (int num : freq) {
+                if (num % 2 == 1)
+                    maxOdd = Math.max(num, maxOdd);
+                else
+                    maxEven = Math.min(num == 0 ? s.length() : num, maxEven);
+            }
+            return maxOdd - maxEven;
+        }
+    }
+}
