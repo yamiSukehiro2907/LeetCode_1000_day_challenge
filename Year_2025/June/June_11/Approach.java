6e7ef383e6eb861a6e9feec9bf7bee5baee10c80
Vimal290704 <vimalyadavkr001@gmail.com>
2025-06-12 00:50:39 +0530

    LeetCode Daily Question June 11

--- Year_2025/June/June_11/Approach.java
+++ Year_2025/June/June_11/Approach.java
@@ -3,49 +3,85 @@ import java.util.*;
 public class Approach {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.maxDifference("1122211", 3));
+        System.out.println("Max Difference: " + solution.maxDifference("2222130", 2));
     }
 
     static class Solution {
-        private Map<Integer, Integer> oddMap;
-        private Map<Integer, Integer> evenMap;
-        private int left, right;
+        private static final int BAD = Integer.MAX_VALUE / 2;
 
         public int maxDifference(String s, int k) {
-            this.oddMap = new HashMap<>();
-            this.evenMap = new HashMap<>();
-            this.left = 0;
-            this.right = 0;
-            for (; right < k; right++) {
-                fill(s.charAt(right));
-            }
-            while (left + k < right) {
-                if (shouldFill()) {
-                    fill(s.charAt(++right));
+            char[] c = s.toCharArray();
+            int n = c.length;
+            int[] minAB = new int[4];
+            int[] minBA = new int[4];
+            boolean[] skip = new boolean[5];
+            int res = Integer.MIN_VALUE;
+            outer: for (char a = '1'; a <= '4'; a++) {
+                inner: for (char b = '0'; b < a; b++) {
+                    if (skip[b - '0'])
+                        continue;
+                    int r = 0, ar = 0, br = 0;
+                    while (r < k || ar + br < 3 || ar == 0 || br == 0) {
+                        if (r == n) {
+                            if (br == 0)
+                                skip[b - '0'] = true;
+                            if (ar == 0) {
+                                skip[a - '0'] = true;
+                                continue outer;
+                            } else
+                                continue inner;
+                        }
+                        char ch = c[r++];
+                        if (ch == a)
+                            ar++;
+                        else if (ch == b)
+                            br++;
+                    }
+                    int l = 0, al = 0, bl = 0;
+                    Arrays.fill(minAB, BAD);
+                    Arrays.fill(minBA, BAD);
+                    minAB[0] = minBA[0] = 0;
+                    while (true) {
+                        while (l < r - k) {
+                            char ch = c[l++];
+                            if (ch == a) {
+                                if (ar == al + 1) {
+                                    l--;
+                                    break;
+                                }
+                                al++;
+                            } else if (ch == b) {
+                                if (br == bl + 1) {
+                                    l--;
+                                    break;
+                                }
+                                bl++;
+                            } else
+                                continue;
+                            int p = ((al & 1) << 1) + (bl & 1);
+                            int d = al - bl;
+                            minAB[p] = Math.min(minAB[p], d);
+                            minBA[p] = Math.min(minBA[p], -d);
+                        }
+                        int p = ((ar & 1) << 1) + (br & 1);
+                        int d = ar - br;
+                        res = Math.max(res, d - minAB[p ^ 2]);
+                        res = Math.max(res, -d - minBA[p ^ 1]);
+                        if (r == n)
+                            break;
+                        char ch = c[r++];
+                        if (ch == a)
+                            ar++;
+                        else if (ch == b)
+                            br++;
+                        else {
+                            while (r < n && (ch = c[r]) != a && ch != b)
+                                r++;
+                        }
+                    }
                 }
             }
-            return 0;
-        }
-
-        private void fill(char ch) {
-            boolean oddContains = oddMap.containsKey(ch - '0');
-            boolean evenContains = evenMap.containsKey(ch - '0');
-            if (!oddContains && !evenContains) {
-                oddMap.put(ch - '0', 1);
-            } else if (oddContains) {
-                evenMap.put(ch - '0', oddMap.get(ch - '0') + 1);
-                oddMap.remove(ch - '0');
-            } else {
-                oddMap.put(ch - '0', evenMap.get(ch - '0') + 1);
-                evenMap.remove(ch - '0');
-            }
-        }
-
-        private boolean shouldFill() {
-            if (oddMap.isEmpty() || evenMap.isEmpty()) {
-                return true;
-            }
-            return false;
+            return res;
         }
     }
 }
