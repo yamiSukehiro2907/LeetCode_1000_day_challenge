bf28cfe29c33429346a9aae20acabcba2b78ba5c
Vimal290704 <vimalyadavkr001@gmail.com>
2025-06-15 20:30:55 +0530

    daily question leetcode june 15

---
+++ Year_2025/June/June_15/Approach.java
@@ -0,0 +1,49 @@
+public class Approach {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        private int difference(int num) {
+            int firstNonNine = -1, firstNonOne = -1, firstDigit = -1;
+            int remaining = num;
+            while (remaining > 0) {
+                int digit = remaining % 10;
+                if (digit != 9) {
+                    firstNonNine = digit;
+                }
+                if (digit > 1) {
+                    firstNonOne = digit;
+                }
+                firstDigit = digit;
+                remaining /= 10;
+            }
+            remaining = num;
+            int min = 0, max = 0;
+            int multiplier = 1;
+            while (remaining > 0) {
+                int digit = remaining % 10;
+                int minDigit = digit;
+                int maxDigit = digit;
+                if (firstDigit == 1 && digit == firstNonOne) {
+                    minDigit = 0;
+                }
+                if (firstDigit != 1 && digit == firstDigit) {
+                    minDigit = 1;
+                }
+                if (digit == firstNonNine) {
+                    maxDigit = 9;
+                }
+                min += multiplier * minDigit;
+                max += multiplier * maxDigit;
+                multiplier *= 10;
+                remaining /= 10;
+            }
+            return max - min;
+        }
+
+        public int maxDiff(int num) {
+            return difference(num);
+        }
+    }
+}
