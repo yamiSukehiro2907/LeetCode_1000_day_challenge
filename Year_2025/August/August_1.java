c35be6dd6d5fa3e9e708cf6b1a91f46e116bc1ad
Vimal290704 <vimalyadavkr001@gmail.com>
2025-08-01 22:42:25 +0530

    leetcode daily question

---
+++ Year_2025/August/August_1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class August_1 {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        void fun(List<List<Integer>> list, int i, int n) {
+            List<Integer> li = new ArrayList<>();
+            if (i >= n) {
+                return;
+            }
+            if (i == 0) {
+                li.add(1);
+            } else {
+                li.add(1);
+                for (int j = 1; j < i; j++) {
+                    li.add(list.get(i - 1).get(j - 1) + list.get(i - 1).get(j));
+                }
+                li.add(1);
+            }
+            list.add(li);
+            fun(list, i + 1, n);
+        }
+
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> list = new ArrayList<>();
+            fun(list, 0, numRows);
+            return list;
+        }
+    }
+}
