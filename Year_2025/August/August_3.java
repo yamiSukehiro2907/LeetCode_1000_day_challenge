90775a3db51906b70f239e353195d2f941e95920
Vimal290704 <vimalyadavkr001@gmail.com>
2025-08-03 23:04:10 +0530

    leetcode daily question 3 august

---
+++ Year_2025/August/August_3.java
@@ -0,0 +1,35 @@
+public class August_3 {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public int maxTotalFruits(int[][] fruits, int start, int k) {
+            int n = fruits.length;
+            int left = lowerBound(fruits, start - k);
+            int right = left;
+            int sum = 0;
+            for (; right < n && fruits[right][0] <= start; right++) sum += fruits[right][1];
+            int end = sum;
+            for (; right < n && fruits[right][0] <= start + k; right++) {
+                sum += fruits[right][1];
+                while ((fruits[right][0] - start + fruits[right][0] - fruits[left][0]) > k && (start - fruits[left][0] + fruits[right][0] - fruits[left][0]) > k) {
+                    sum -= fruits[left][1];
+                    left++;
+                }
+                end = Math.max(end, sum);
+            }
+            return end;
+        }
+
+        private int lowerBound(int[][] arr, int target) {
+            int low = 0, high = arr.length;
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                if (arr[mid][0] < target) low = mid + 1;
+                else high = mid;
+            }
+            return low;
+        }
+    }
+}
