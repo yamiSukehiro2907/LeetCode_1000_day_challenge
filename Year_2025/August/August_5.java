c30f1903d459a60630d538f1adf978236c1f868f
Vimal290704 <vimalyadavkr001@gmail.com>
2025-08-05 13:28:06 +0530

    LeetCode daily question august 5

---
+++ Year_2025/August/August_5.java
@@ -0,0 +1,21 @@
+public class August_5 {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+            int count = 0;
+            for (int fruit : fruits) {
+                for (int i = 0; i < baskets.length; i++) {
+                    if (fruit <= baskets[i]) {
+                        count++;
+                        baskets[i] = 0;
+                        break;
+                    }
+                }
+            }
+            return fruits.length - count;
+        }
+    }
+}
