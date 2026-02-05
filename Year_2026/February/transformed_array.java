eb7e3707c885b2ff6481832b41bfad813c31211a
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2026-02-05 17:35:36 +0530

    Add transformed_array.java for array transformation
    daily leetcode question of transforming array

---
+++ Year_2026/February/transformed_array.java
@@ -0,0 +1,17 @@
+package February;
+
+public class transformed_array{
+  public static void main(String[] args){
+    Solution solution = new Solution();
+    int[] arr = {-1 , 2 , -4 , 4};
+    System.out.println(Arrays.toString(solution.constructTransformedArray(arr)));
+  }
+
+  static class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i = 0 ; i < nums.length ; i++) ans[i] = nums[((i + nums[i]) % nums.length + nums.length) % nums.length];
+        return ans;
+    }
+  }
+}
