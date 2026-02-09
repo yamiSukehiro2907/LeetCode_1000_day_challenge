d6b8634ab9a5e1483989f0613fb41757f38a78e6
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2026-02-09 11:56:28 +0530

    Add TreeNode class for binary tree structure

---
+++ Year_2026/February/TreeNode.java
@@ -0,0 +1,13 @@
+package February;
+
+public class TreeNode {
+   public int val;
+   public TreeNode left;
+   public TreeNode right;
+
+   public TreeNode(int _val){
+      this.val = _val;
+      this.left = null;
+      this.right = null;
+   }
+}
